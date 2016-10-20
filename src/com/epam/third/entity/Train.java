5e5bfdfa543e99f4e8fd95b0be32f129b8f7a95b
mockturtleqt <galia.semashko@gmail.com>
2016-10-20 23:50:38 +0300

    Logic changed

--- src/com/epam/third/entity/Train.java
+++ src/com/epam/third/entity/Train.java
@@ -1,13 +1,8 @@
 package com.epam.third.entity;
 
 import com.epam.third.service.TunnelManager;
-import org.apache.log4j.Logger;
-
-import java.time.LocalTime;
-import java.util.concurrent.TimeUnit;
 
 public class Train extends Thread {
-    private static Logger logger = Logger.getLogger(Train.class);
     private TrainDirection direction;
     private String trainName;
 
